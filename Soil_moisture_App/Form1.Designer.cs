7e6e8f539465e38f66c9429c98ed36f77a1ae075
MatthiasW <MatthiasWagner@zoho.com>
2015-09-04 23:24:32 +0800

    Initial commit
    First commit of Soil Moisture Sensor App

---
+++ Soil_moisture_App/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Soil_moisture_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.conBTN = new System.Windows.Forms.Button();
+            this.disconBTN = new System.Windows.Forms.Button();
+            this.caliBTN = new System.Windows.Forms.Button();
+            this.comPort_comboBox = new System.Windows.Forms.ComboBox();
+            this.sendBTN = new System.Windows.Forms.Button();
+            this.txtReceiveBox = new System.Windows.Forms.TextBox();
+            this.txtDataSendBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maxMoisLab = new System.Windows.Forms.Label();
+            this.minMoisLab = new System.Windows.Forms.Label();
+            this.moisVoltLab = new System.Windows.Forms.Label();
+            this.moisLab = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // conBTN
+            // 
+            this.conBTN.Location = new System.Drawing.Point(12, 62);
+            this.conBTN.Name = "conBTN";
+            this.conBTN.Size = new System.Drawing.Size(116, 58);
+            this.conBTN.TabIndex = 0;
+            this.conBTN.Text = "Connect";
+            this.conBTN.UseVisualStyleBackColor = true;
+            this.conBTN.Click += new System.EventHandler(this.conBTN_Click);
+            // 
+            // disconBTN
+            // 
+            this.disconBTN.Location = new System.Drawing.Point(134, 62);
+            this.disconBTN.Name = "disconBTN";
+            this.disconBTN.Size = new System.Drawing.Size(155, 58);
+            this.disconBTN.TabIndex = 1;
+            this.disconBTN.Text = "Disconnect";
+            this.disconBTN.UseVisualStyleBackColor = true;
+            this.disconBTN.Click += new System.EventHandler(this.disconBTN_Click);
+            // 
+            // caliBTN
+            // 
+            this.caliBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.17801F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.caliBTN.Location = new System.Drawing.Point(12, 126);
+            this.caliBTN.Name = "caliBTN";
+            this.caliBTN.Size = new System.Drawing.Size(277, 95);
+            this.caliBTN.TabIndex = 2;
+            this.caliBTN.Text = "Calibrate";
+            this.caliBTN.UseVisualStyleBackColor = true;
+            this.caliBTN.Click += new System.EventHandler(this.caliBTN_Click);
+            // 
+            // comPort_comboBox
+            // 
+            this.comPort_comboBox.FormattingEnabled = true;
+            this.comPort_comboBox.Location = new System.Drawing.Point(12, 13);
+            this.comPort_comboBox.Name = "comPort_comboBox";
+            this.comPort_comboBox.Size = new System.Drawing.Size(277, 33);
+            this.comPort_comboBox.TabIndex = 3;
+            // 
+            // sendBTN
+            // 
+            this.sendBTN.Location = new System.Drawing.Point(757, 185);
+            this.sendBTN.Name = "sendBTN";
+            this.sendBTN.Size = new System.Drawing.Size(193, 61);
+            this.sendBTN.TabIndex = 4;
+            this.sendBTN.Text = "SEND test ";
+            this.sendBTN.UseVisualStyleBackColor = true;
+            this.sendBTN.Click += new System.EventHandler(this.sendBTN_Click);
+            // 
+            // txtReceiveBox
+            // 
+            this.txtReceiveBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtReceiveBox.Font = new System.Drawing.Font("Consolas", 7.916231F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReceiveBox.ForeColor = System.Drawing.Color.PaleGreen;
+            this.txtReceiveBox.Location = new System.Drawing.Point(336, 13);
+            this.txtReceiveBox.Multiline = true;
+            this.txtReceiveBox.Name = "txtReceiveBox";
+            this.txtReceiveBox.Size = new System.Drawing.Size(614, 166);
+            this.txtReceiveBox.TabIndex = 5;
+            // 
+            // txtDataSendBox
+            // 
+            this.txtDataSendBox.Location = new System.Drawing.Point(336, 185);
+            this.txtDataSendBox.Name = "txtDataSendBox";
+            this.txtDataSendBox.Size = new System.Drawing.Size(415, 31);
+            this.txtDataSendBox.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.maxMoisLab);
+            this.groupBox1.Controls.Add(this.minMoisLab);
+            this.groupBox1.Controls.Add(this.moisVoltLab);
+            this.groupBox1.Controls.Add(this.moisLab);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.916231F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 251);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 213);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Values";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 7.162304F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 35);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "volt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 7.162304F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "moist";
+            // 
+            // maxMoisLab
+            // 
+            this.maxMoisLab.AutoSize = true;
+            this.maxMoisLab.Font = new System.Drawing.Font("Segoe Print", 7.162304F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxMoisLab.Location = new System.Drawing.Point(138, 172);
+            this.maxMoisLab.Name = "maxMoisLab";
+            this.maxMoisLab.Size = new System.Drawing.Size(72, 35);
+            this.maxMoisLab.TabIndex = 0;
+            this.maxMoisLab.Text = "max: ";
+            // 
+            // minMoisLab
+            // 
+            this.minMoisLab.AutoSize = true;
+            this.minMoisLab.Font = new System.Drawing.Font("Segoe Print", 7.162304F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minMoisLab.Location = new System.Drawing.Point(11, 172);
+            this.minMoisLab.Name = "minMoisLab";
+            this.minMoisLab.Size = new System.Drawing.Size(68, 35);
+            this.minMoisLab.TabIndex = 0;
+            this.minMoisLab.Text = "min: ";
+            // 
+            // moisVoltLab
+            // 
+            this.moisVoltLab.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.moisVoltLab.Font = new System.Drawing.Font("Segoe Print", 18.09424F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moisVoltLab.Location = new System.Drawing.Point(87, 88);
+            this.moisVoltLab.Name = "moisVoltLab";
+            this.moisVoltLab.Size = new System.Drawing.Size(178, 80);
+            this.moisVoltLab.TabIndex = 0;
+            this.moisVoltLab.Text = "--";
+            this.moisVoltLab.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // moisLab
+            // 
+            this.moisLab.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.moisLab.Font = new System.Drawing.Font("Segoe Print", 18.09424F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moisLab.Location = new System.Drawing.Point(87, 25);
+            this.moisLab.Name = "moisLab";
+            this.moisLab.Size = new System.Drawing.Size(178, 80);
+            this.moisLab.TabIndex = 0;
+            this.moisLab.Text = "--";
+            this.moisLab.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 133);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 480);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtDataSendBox);
+            this.Controls.Add(this.txtReceiveBox);
+            this.Controls.Add(this.sendBTN);
+            this.Controls.Add(this.comPort_comboBox);
+            this.Controls.Add(this.caliBTN);
+            this.Controls.Add(this.disconBTN);
+            this.Controls.Add(this.conBTN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Soil moisture App";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button conBTN;
+        private System.Windows.Forms.Button disconBTN;
+        private System.Windows.Forms.Button caliBTN;
+        private System.Windows.Forms.ComboBox comPort_comboBox;
+        private System.Windows.Forms.Button sendBTN;
+        private System.Windows.Forms.TextBox txtReceiveBox;
+        private System.Windows.Forms.TextBox txtDataSendBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label moisLab;
+        private System.Windows.Forms.Label maxMoisLab;
+        private System.Windows.Forms.Label minMoisLab;
+        private System.Windows.Forms.Label moisVoltLab;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Soil_moisture_App/Form1.cs
@@ -0,0 +1,341 @@
+﻿#define DEMO
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Soil_moisture_App
+{
+    public struct cmdStruct
+    {
+        public byte cmd, val1, val2;
+    }
+
+    public partial class Form1 : Form
+    {
+        public SynchronizationContext mainThread;
+        public System.IO.Ports.SerialPort sport;
+        public CalibrationForm CaliForm;
+
+        public volatile bool _backgroundPause = true;
+
+        public string[] cmdArray = {
+            "CMD 1",
+            "CMD 2",
+            "..."
+        };
+
+        public struct cmdStruct
+        {
+            public byte cmd;
+            public byte val1;
+            public byte val2;
+        } ;
+
+        enum FormComModes : byte
+        {   CaliStart,
+            CaliDry,
+            CaliWet,
+            CaliFin,
+            CaliError,
+            CaliEnd
+        };
+
+        enum CMDs : byte
+        {   CMD_MOIS = (byte)'A',
+            CMD_VOLT = (byte)'B',
+            CMD_MIN,
+            CMD_MAX,
+            CMD_CALI,
+            CMD_DRY,
+            CMD_WET,
+            CMD_FIN,
+            CMD_TEST
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            disconBTN.Enabled = false;
+            caliBTN.Enabled = false;
+            foreach (String s in System.IO.Ports.SerialPort.GetPortNames())
+                comPort_comboBox.Items.Add(s);
+
+            //debug code
+            if (comPort_comboBox.Items.Contains("COM2"))
+                comPort_comboBox.Text = "COM2";
+
+            moisLab.Text = "55%";
+            moisVoltLab.Text = "2,1V";
+            minMoisLab.Text = "min: 0,82";
+            maxMoisLab.Text = "max: 2,30";
+
+            mainThread = SynchronizationContext.Current;
+            if (mainThread == null) mainThread = new SynchronizationContext();
+
+        }
+
+
+        public void serialport_connect(String port, int baudrate, Parity parity, int databits, StopBits stopbits)
+        {
+            if (port != "") { 
+                sport = new System.IO.Ports.SerialPort(
+                port, baudrate, parity, databits, stopbits);
+
+                try
+                {
+                    sport.Open();
+                    disconBTN.Enabled = true;
+                    conBTN.Enabled = false;
+                    caliBTN.Enabled = true;
+                    txtReceiveBox.AppendText("[" + get_dtn() + "] " + "Connected\n");
+                    sport.DataReceived += new SerialDataReceivedEventHandler(sport_DataReceived);
+
+                    // Create the thread object. This does not start the thread.
+                    Thread t = new Thread(new ThreadStart(BackgroundThread));
+                    t.IsBackground = true;
+                    t.Start();
+                    txtReceiveBox.AppendText("[" + get_dtn() + "] " + "Background Thread started\n");
+                    _backgroundPause = true;
+                }
+                catch (Exception ex) { MessageBox.Show(ex.ToString(), "Error"); }}
+
+            
+        }
+
+        public void send_command(byte cmd, byte val1, byte val2)
+        {
+            byte[] b = new byte[3];
+            b[0] = cmd;
+            b[1] = val1;
+            b[2] = val2;
+            sport.Write(b, 0, 3);
+
+            mainThread.Send((object state) =>
+            {
+                txtReceiveBox.AppendText("[" + get_dtn() + "] " + "cmd: " + cmd.ToString() + "  val1: " + val1.ToString() + "  val2: " + val2.ToString() + "\n");
+            }, null);
+        }
+
+        public String get_dtn()
+        {
+            DateTime dt = DateTime.Now;
+            //return dt.ToShortTimeString();
+            return dt.ToLongTimeString();
+        }
+
+        public void CaliFormCallback(byte m)
+        {
+            button1.Text = m.ToString();
+            if (m == (byte)FormComModes.CaliStart)
+            {
+                send_command((byte)CMDs.CMD_CALI, 1, 0);
+                Thread.Sleep(2000);
+                #if DEMO
+                CaliForm.changeContext((byte)FormComModes.CaliDry);
+                #endif
+            }
+            else if (m == (byte)FormComModes.CaliDry)
+            {
+                send_command((byte)CMDs.CMD_DRY, 1, 0);
+                Thread.Sleep(2000);
+                #if DEMO
+                CaliForm.changeContext((byte)FormComModes.CaliWet);
+                #endif
+            }
+            else if (m == (byte)FormComModes.CaliWet)
+            {
+                send_command((byte)CMDs.CMD_WET, 1, 0);
+                Thread.Sleep(2000);
+                bool result = false;
+                if (result)
+                {
+                    #if DEMO
+                    CaliForm.changeContext((byte)FormComModes.CaliFin);
+                    #endif
+                    _backgroundPause = false;
+                }
+                else
+                {
+                    #if DEMO
+                    CaliForm.changeContext((byte)FormComModes.CaliError);
+                    #endif
+                    _backgroundPause = false;
+                }
+
+            }
+            else if ((m == (byte)FormComModes.CaliFin) || (m == (byte)FormComModes.CaliError))
+                CaliForm.Close();
+
+        }
+
+        private cmdStruct decodeReceivedCmd(string str)
+        {
+            // Protocoll
+            // |  CMD  |  val1 |  val2 |  \n  |
+            // |  byte |  byte |  byte | byte |
+            // 
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            cmdStruct cmdBack = new cmdStruct();
+            cmdBack.cmd = bytes[0];
+            cmdBack.val1 = bytes[1];
+            cmdBack.val2 = bytes[2];
+            return cmdBack;
+        }
+
+        private void processReceivedCmd(cmdStruct c)
+        {
+            switch (c.cmd)
+            {
+                case (byte)CMDs.CMD_MOIS:
+                    moisLab.Text = Convert.ToString((c.val1 << 8) + c.val2);
+                    txtReceiveBox.AppendText("[" + get_dtn() + "] " + " received MOIS" + "\n");
+
+                    break;
+                case (byte)CMDs.CMD_VOLT:
+                    moisVoltLab.Text = Convert.ToString((int)((byte)((c.val1 << 8) + c.val2)) / 100);
+                    break;
+                case (byte)CMDs.CMD_MIN:
+                    minMoisLab.Text = Convert.ToString((int)((byte)((c.val1 << 8) + c.val2))/100);
+                    break;
+                case (byte)CMDs.CMD_MAX:
+                    maxMoisLab.Text = Convert.ToString((int)((byte)((c.val1 << 8) + c.val2)) / 100);
+                    break;
+                case (byte)CMDs.CMD_CALI:
+                    if((byte)c.val1 == 1)
+                        CaliForm.changeContext((byte)FormComModes.CaliDry);
+                    else
+                        CaliForm.changeContext((byte)FormComModes.CaliError);
+                    break;
+                case (byte)CMDs.CMD_DRY:
+                    if ((byte)c.val1 == 1)
+                        CaliForm.changeContext((byte)FormComModes.CaliWet);
+                    else
+                        CaliForm.changeContext((byte)FormComModes.CaliError);
+                    break;
+                case (byte)CMDs.CMD_WET:
+                    if ((byte)c.val1 == 1)
+                        CaliForm.changeContext((byte)FormComModes.CaliFin);
+                    else
+                        CaliForm.changeContext((byte)FormComModes.CaliError);
+                    break;
+                case (byte)CMDs.CMD_TEST:
+                    moisLab.Text = Convert.ToString((int)((byte)((c.val1 << 8) + c.val2)) / 100);
+                    txtReceiveBox.AppendText("[" + get_dtn() + "] " + " received TEST" + "\n");
+                    _backgroundPause = false;
+                    break;
+                    /*
+                case (byte)CMDs.CMD_FIN:
+                    if ((byte)c.val1 == 1)
+                        CaliForm.changePic((byte)FormComModes.CaliFin);
+                    else
+                        CaliForm.changePic((byte)FormComModes.CaliError);
+                    break;
+                     * */
+                default:
+                    break;
+            }
+        }
+
+        private void sport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String str = sport.ReadExisting();
+
+            cmdStruct cmdBack = decodeReceivedCmd(str);
+            mainThread.Send((object state) =>
+            {
+                processReceivedCmd(cmdBack);
+                txtReceiveBox.AppendText("[" + get_dtn() + "] " + "Received: cmd: " + cmdBack.cmd.ToString() + "  val1: " + cmdBack.val1.ToString() + "  val2: " + cmdBack.val2.ToString() + "\n");
+            }, null);
+        }
+
+
+        private void conBTN_Click(object sender, EventArgs e)
+        {
+            String port = comPort_comboBox.Text;
+            int baudrate = 9600;
+            Parity parity = System.IO.Ports.Parity.None;
+            int databits = 8;
+            StopBits stopbits = System.IO.Ports.StopBits.One;
+            
+            serialport_connect(port, baudrate, parity, databits, stopbits);
+            byte[] c = new byte[4];
+            c[0] = (byte)'t';
+            c[1] = (byte)'e';
+            c[2] = (byte)'s';
+            c[3] = (byte)'t';
+
+            sport.Write(c,0,c.Count());
+            //send_command((byte)'A', (byte)'A', (byte)'a');
+        }
+
+        private void sendBTN_Click(object sender, EventArgs e)
+        {
+            String data = txtDataSendBox.Text;
+            sport.Write(data);
+            txtReceiveBox.AppendText("[" + get_dtn() + "] " + "Sent: " + data + "\n");
+        }
+
+        private void disconBTN_Click(object sender, EventArgs e)
+        {
+            if (sport.IsOpen)
+            {
+                sport.Close();
+                disconBTN.Enabled = false;
+                caliBTN.Enabled = false;
+                conBTN.Enabled = true;
+                txtReceiveBox.AppendText("[" + get_dtn() + "] " + "Disconnected\n");
+            }
+        }
+
+        private void caliBTN_Click(object sender, EventArgs e)
+        {
+            _backgroundPause = true;
+            txtReceiveBox.AppendText("[" + get_dtn() + "] " + "Start Calibration!\n");
+            CaliForm = new CalibrationForm(this);
+            CaliForm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] a = new byte[1];
+            a[0] = 40;
+            //button1.Text = a;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        public void BackgroundThread()
+        {
+            int runs = 0;
+            while (true)
+            {
+                while (!_backgroundPause)
+                {
+                    Thread.Sleep(5000);
+                    mainThread.Send((object state) =>
+                    {
+                        txtReceiveBox.AppendText("[" + get_dtn() + "] " + "background thread: working... " + runs++ + "\n");
+                    }, null); 
+                    send_command((byte)CMDs.CMD_MOIS, 1, 0);
+                    _backgroundPause = true;
+                }
+                Console.WriteLine("worker thread: terminating gracefully.");
+            }
+        }
+    }
+}
+
+
+
